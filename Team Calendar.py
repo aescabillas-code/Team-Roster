66012971d556aff5ac43512666e1b2866f2b5c57
aescabillas-code <aescabillas@gmail.com>
2026-07-15 10:28:15 +0800

    Team Calendar.py

--- Team Calendar.py
+++ Team Calendar.py
@@ -483,7 +483,7 @@ with tab_cal:
         st.divider()
         
         st.write("**Today's Schedule:**")
-        roles = ["team_manager","call", "chat", "mfq", "sme"]
+        roles = ["call", "chat", "mfq", "sme"]
 
         
         approved_requests = fetch_approved_requests_from_db()
@@ -500,7 +500,7 @@ with tab_cal:
                 st.write(f"- **{name}**: {p_status[0].upper()}")
             else:
                 assigned_roles = [
-                    r.upper().replace("_", " ")
+                    r.upper()
                     for r in roles
                     if name in d_data.get(r, [])]
             
@@ -1807,7 +1807,6 @@ with tab_adm:
 
             available = [n for n in roster.keys() if n not in unavailable] if roster else []
             
-            team_manager = st.selectbox("Assign Team Manager", [""] + available, key="sb_assign_team_manager")
             call = st.multiselect("Assign Call", available, key="ms_assign_call")
             chat = st.multiselect("Assign Chat", available, key="ms_assign_chat")
             mfq = st.multiselect("Assign MFQ", available, key="ms_assign_mfq")
@@ -1818,7 +1817,6 @@ with tab_adm:
                     st.session_state.calendar_data[d] = {
                         "shift": shift_display, 
                         "status": setup, 
-                        "team_manager": [team_manager] if team_manager else [],
                         "call": call, 
                         "chat": chat,
                         "mfq": mfq,
