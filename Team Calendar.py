af5dd2042e3b639eeada1f2a1e0bed81186448d1
aescabillas-code <aescabillas@gmail.com>
2026-07-19 16:13:04 +0800

    Update Team Calendar.py

--- Team Calendar.py
+++ Team Calendar.py
@@ -318,6 +318,10 @@ tab_names = [
     "🔍 Case Tracker", "🔀 Deviation", "🔑 Admin"
 ]
 
+tab_cal, tab_req, tab_prod, tab_case, tab_dev, tab_adm = st.tabs(
+    tab_names, 
+    key="active_tab"
+
 # Initialize state tracker
 if "active_tab" not in st.session_state:
     st.session_state.active_tab = tab_names[0]
